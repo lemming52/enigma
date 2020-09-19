f94c366d3c640705214f8089431052f9fadefd3c
Andrew Strange <andrew@gumboot.co.uk>
2020-09-19 19:53:46 +0100

    Add Rotor object
    Add a rotor object with traversal and cycling methods, along with
    initialisation from a string.

---
+++ enigma/rotor.go
@@ -0,0 +1,66 @@
+package enigma
+
+import (
+	"fmt"
+)
+
+type Rotor struct {
+	Name        string
+	connections *[2][26]int
+	position    int
+}
+
+const runeOffset = 65
+
+// NewRotor takes a configuration string of 26 characters and instantiates a rotor object
+func NewRotor(name, configuration string, position int) (*Rotor, error) {
+	connections, err := convertStringConfiguration(configuration)
+	if err != nil {
+		msg := fmt.Sprintf("Unable to create rotor: %v", err)
+		fmt.Println(msg)
+		return nil, err
+	}
+	return &Rotor{
+		Name:        name,
+		connections: connections,
+		position:    position,
+	}, nil
+}
+
+// Traverse passes a signal through the rotor configuration, either forwards or backwards
+func (r *Rotor) Traverse(position int, forwards bool) int {
+	if forwards {
+		return r.connections[0][(position+r.position)%25]
+	} else {
+		return r.connections[1][(position+r.position)%25]
+	}
+}
+
+// Cycle rotates the rotor by one position
+func (r *Rotor) Cycle() {
+	r.position = (r.position + 1) % 26
+}
+
+// convertStringConfiguration converts a single string of characters, representing what characters [A-Z] map to
+// in position 0, and returns a slice of the wire pairs
+func convertStringConfiguration(conf string) (*[2][26]int, error) {
+	connections := &[2][26]int{}
+	runes := []rune(conf)
+	for i, r := range runes {
+		if !isAllowedCharacter(r) {
+			return nil, fmt.Errorf("Forbidden Character in configuration: %b", conf[i])
+		}
+		position := int(r - runeOffset)
+		connections[0][i] = position
+		if i != 0 && connections[1][position] != 0 {
+			return nil, fmt.Errorf("Duplicate Character map, characters in positions %d and %d", i, connections[1][position])
+		}
+		connections[1][position] = i
+	}
+	return connections, nil
+}
+
+// isAllowedCharacter constrains the configuration string to uppercase letters
+func isAllowedCharacter(r rune) bool {
+	return ('A' <= r && r <= 'Z')
+}
---
+++ enigma/rotor_test.go
@@ -0,0 +1,131 @@
+package enigma
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertStringConfiguration(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected *[2][26]int
+	}{
+		{
+			name:  "base",
+			input: "BCDEFGHIJKLMNOPQRSTUVWXYZA",
+			expected: &[2][26]int{
+				0: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0},
+				1: {25, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24},
+			},
+		}, {
+			name:  "rotor I",
+			input: "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+			expected: &[2][26]int{
+				0: {4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9},
+				1: {20, 22, 24, 6, 0, 3, 5, 15, 21, 25, 1, 4, 2, 10, 12, 19, 7, 23, 18, 11, 17, 8, 13, 16, 14, 9},
+			},
+		}, {
+			name:  "reflector",
+			input: "YRUHQSLDPXNGOKMIEBFZCWVJAT",
+			expected: &[2][26]int{
+				0: {24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19},
+				1: {24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19},
+			},
+		},
+	}
+	for _, test := range tests {
+		tt := test
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			res, err := convertStringConfiguration(tt.input)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.expected, res, "expected arrays should be equal")
+		})
+	}
+}
+
+func TestCycle(t *testing.T) {
+	tests := []struct {
+		name     string
+		startPos int
+		endPos   int
+	}{
+		{
+			name:     "base",
+			startPos: 5,
+			endPos:   6,
+		}, {
+			name:     "overflow",
+			startPos: 25,
+			endPos:   0,
+		},
+	}
+	for _, test := range tests {
+		tt := test
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			r := Rotor{position: tt.startPos}
+			r.Cycle()
+			assert.Equal(t, tt.endPos, r.position, "rotor did not cycle correctly, got %d expected %d", r.position, tt.endPos)
+		})
+	}
+}
+
+func TestTraverse(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       int
+		startPos    int
+		connections *[2][26]int
+		expected    int
+		forwards    bool
+	}{
+		{
+			name:     "base",
+			input:    5,
+			startPos: 5,
+			connections: &[2][26]int{
+				0: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0},
+				1: {25, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24},
+			},
+			expected: 11,
+			forwards: true,
+		},
+		{
+			name:     "overflow",
+			input:    10,
+			startPos: 20,
+			connections: &[2][26]int{
+				0: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0},
+				1: {25, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24},
+			},
+			expected: 6,
+			forwards: true,
+		},
+		{
+			name:     "backwards",
+			input:    5,
+			startPos: 5,
+			connections: &[2][26]int{
+				0: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0},
+				1: {25, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24},
+			},
+			expected: 9,
+			forwards: false,
+		},
+	}
+	for _, test := range tests {
+		tt := test
+		t.Run(tt.name, func(t *testing.T) {
+			r := Rotor{
+				connections: tt.connections,
+				position:    tt.startPos,
+			}
+			t.Parallel()
+			res := r.Traverse(tt.input, tt.forwards)
+			assert.Equal(t, tt.expected, res, "output value should match expected")
+		})
+	}
+}
